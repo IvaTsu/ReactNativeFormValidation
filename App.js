e11ae5e2858a171715d6c6a21aee230cb8a58bf9
IvaTsu <ivan.tsud@gmail.com>
2018-12-25 21:54:28 +0200

    Refactor 'App.js'

--- App.js
+++ App.js
@@ -4,45 +4,14 @@
  */
 
 import React, { Fragment } from 'react';
-import { SafeAreaView, TextInput, Button, ActivityIndicator, Text } from 'react-native';
-import { Formik } from 'formik';
-import * as yup from 'yup';
+import { SafeAreaView } from 'react-native';
 
-import SignIn from './src/forms/SignIn';
-
-const validationSchema = yup.object().shape({
-	name: yup.string().required('Required')
-});
+import OneFieldForm from './src/forms/OneFieldForm/index';
 
 export default () => (
 	<SafeAreaView>
-		<Formik
-			initialValues={{ name: '' }}
-			// actions should be Formik functions to change state of formikProps
-			onSubmit={(values, actions) => {
-				// setTimeout just for latency
-				setTimeout(() => {
-					actions.setSubmitting(false);
-				}, 1000);
-				alert(JSON.stringify(values));
-			}}
-			validationSchema={validationSchema}
-		>
-			{formikProps => (
-				<Fragment>
-					<TextInput
-						style={{ borderWidth: 2, margin: 10, padding: 5 }}
-						onChangeText={formikProps.handleChange('name')}
-					/>
-					<Text style={{ color: 'red' }}>{formikProps.errors.name}</Text>
-					{formikProps.isSubmitting ? (
-						<ActivityIndicator />
-					) : (
-						<Button title="Submit" onPress={formikProps.handleSubmit} />
-					)}
-				</Fragment>
-			)}
-		</Formik>
-		<SignIn />
+		<Fragment>
+			<OneFieldForm />
+		</Fragment>
 	</SafeAreaView>
 );
---
+++ src/forms/OneFieldForm/index.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react';
+import { SafeAreaView, TextInput, Button, ActivityIndicator, Text } from 'react-native';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const validationSchema = yup.object().shape({
+	name: yup.string().required('Required')
+});
+
+export default () => (
+	<SafeAreaView>
+		<Formik
+			initialValues={{ name: '' }}
+			// actions should be Formik functions to change state of formikProps
+			onSubmit={(values, actions) => {
+				// setTimeout just for latency
+				setTimeout(() => {
+					actions.setSubmitting(false);
+				}, 1000);
+				alert(JSON.stringify(values));
+			}}
+			validationSchema={validationSchema}
+		>
+			{formikProps => (
+				<Fragment>
+					<TextInput
+						style={{ borderWidth: 2, margin: 10, padding: 5 }}
+						onChangeText={formikProps.handleChange('name')}
+						onBlur={formikProps.handleBlur('name')}
+					/>
+					<Text style={{ color: 'red' }}>{formikProps.errors.name}</Text>
+					{formikProps.isSubmitting ? (
+						<ActivityIndicator />
+					) : (
+						<Button title="Submit" onPress={formikProps.handleSubmit} />
+					)}
+				</Fragment>
+			)}
+		</Formik>
+	</SafeAreaView>
+);
