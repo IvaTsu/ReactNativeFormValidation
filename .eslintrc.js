608d253e295d84a15bb433e2ec73d79aa074542e
IvaTsu <ivan.tsud@gmail.com>
2018-12-25 18:13:46 +0200

    Add 'ESlint' and 'Prettier' integration

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	extends: ['prettier'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': ['error']
+	},
+	parser: 'babel-eslint'
+};
--- App.js
+++ App.js
@@ -3,51 +3,43 @@
  * @flow
  */
 
-import React, { Fragment } from "react";
-import {
-  StyleSheet,
-  SafeAreaView,
-  TextInput,
-  Button,
-  ActivityIndicator,
-  Text
-} from "react-native";
-import { Formik } from "formik";
-import * as yup from "yup";
-import { red } from "ansi-colors";
+import React, { Fragment } from 'react';
+import { SafeAreaView, TextInput, Button, ActivityIndicator, Text } from 'react-native';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 const validationSchema = yup.object().shape({
-  name: yup.string().required("Required")
+	name: yup.string().required('Required')
 });
 
 export default () => (
-  <SafeAreaView>
-    <Formik
-      initialValues={{ name: "" }}
-      // actions should be Formik functions to change state of formikProps
-      onSubmit={(values, actions) => {
-        // setTimeout just for latency
-        setTimeout(() => {
-          actions.setSubmitting(false);
-        }, 1000);
-        alert(JSON.stringify(values));
-      }}
-      validationSchema={validationSchema}
-    >
-      {formikProps => (
-        <Fragment>
-          <TextInput
-            style={{ borderWidth: 2, margin: 10, padding: 5 }}
-            onChangeText={formikProps.handleChange("name")}
-          />
-          <Text style={{ color: "red" }}>{formikProps.errors.name}</Text>
-          {formikProps.isSubmitting ? (
-            <ActivityIndicator />
-          ) : (
-            <Button title="Submit" onPress={formikProps.handleSubmit} />
-          )}
-        </Fragment>
-      )}
-    </Formik>
-  </SafeAreaView>
+	<SafeAreaView>
+		<Formik
+			initialValues={{ name: '' }}
+			// actions should be Formik functions to change state of formikProps
+			onSubmit={(values, actions) => {
+				// setTimeout just for latency
+				setTimeout(() => {
+					actions.setSubmitting(false);
+				}, 1000);
+				alert(JSON.stringify(values));
+			}}
+			validationSchema={validationSchema}
+		>
+			{formikProps => (
+				<Fragment>
+					<TextInput
+						style={{ borderWidth: 2, margin: 10, padding: 5 }}
+						onChangeText={formikProps.handleChange('name')}
+					/>
+					<Text style={{ color: 'red' }}>{formikProps.errors.name}</Text>
+					{formikProps.isSubmitting ? (
+						<ActivityIndicator />
+					) : (
+						<Button title="Submit" onPress={formikProps.handleSubmit} />
+					)}
+				</Fragment>
+			)}
+		</Formik>
+	</SafeAreaView>
 );
